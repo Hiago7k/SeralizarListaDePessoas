7a35c9f93ffdf7fd12c0e2700653c7b9ec1d0225
Hiago 7k <hiago7k@gmail.com>
2026-03-24 19:27:46 -0300

    iniciando, projeto

---
+++ Pessoas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeralizarListaDePessoas;
+
+internal class Pessoas
+{
+    List<Usuario> ListaDePessoas = new List<Usuario>();
+    public string Nome { get;  }
+}
